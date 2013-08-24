d8ea24d2e886db6e5daf83e557a5ac7467ba78a0
Edward Kmett <ekmett@gmail.com>
2013-08-24 12:44:44 -0400

    Switched to unshuffled Key in Sparse.Matrix.Key. Other modules as yet unported.

--- src/Sparse/Matrix/Key.hs
+++ src/Sparse/Matrix/Key.hs
@@ -29,16 +29,13 @@
 
 module Sparse.Matrix.Key
   ( Key(..)
-  , key
-  , shuffled, unshuffled
   ) where
 
-import Control.Lens
 import Data.Bits
+import Control.Monad
+import Control.Lens
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Generic.Mutable as GM
-import qualified Data.Vector.Primitive as P
-import qualified Data.Vector.Storable as S
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
 import Data.Word
@@ -48,8 +45,15 @@ import Data.Word
 -- | @key i j@ interleaves the bits of the keys @i@ and @j@.
 --
 -- Keys are then just values sorted in \"Morton Order\".
-newtype Key = Key { runKey :: Word64 }
-  deriving (Eq, Ord, Enum, S.Storable, P.Prim, U.Unbox, GM.MVector UM.MVector, G.Vector U.Vector)
+data Key = Key {-# UNPACK #-} !Word32 {-# UNPACK #-} !Word32
+  deriving (Show, Read, Eq)
+
+instance Ord Key where
+  Key a b `compare` Key c d
+    | ab < cd && ab < xor ab cd = compare b d
+    | otherwise = compare a c
+    where ab = xor a b
+          cd = xor c d
 
 -- | Construct a key from a pair of indices.
 --
@@ -58,105 +62,60 @@ newtype Key = Key { runKey :: Word64 }
 -- key i j ^. _2 = j
 -- @
 key :: Word32 -> Word32 -> Key
-key i j = Key k5 where
-  k0 = unsafeShiftL (fromIntegral i) 32 .|. fromIntegral j
-  k1 = unsafeShiftL (k0 .&. 0x00000000FFFF0000) 16 .|. unsafeShiftR k0 16 .&. 0x00000000FFFF0000 .|. k0 .&. 0xFFFF00000000FFFF
-  k2 = unsafeShiftL (k1 .&. 0x0000FF000000FF00) 8  .|. unsafeShiftR k1 8  .&. 0x0000FF000000FF00 .|. k1 .&. 0xFF0000FFFF0000FF
-  k3 = unsafeShiftL (k2 .&. 0x00F000F000F000F0) 4  .|. unsafeShiftR k2 4  .&. 0x00F000F000F000F0 .|. k2 .&. 0xF00FF00FF00FF00F
-  k4 = unsafeShiftL (k3 .&. 0x0C0C0C0C0C0C0C0C) 2  .|. unsafeShiftR k3 2  .&. 0x0C0C0C0C0C0C0C0C .|. k3 .&. 0xC3C3C3C3C3C3C3C3
-  k5 = unsafeShiftL (k4 .&. 0x2222222222222222) 1  .|. unsafeShiftR k4 1  .&. 0x2222222222222222 .|. k4 .&. 0x9999999999999999
+key = Key
 {-# INLINE key #-}
 
--- | This isomorphism lets you build a key from a pair of indices.
---
--- @
--- key i j ≡ (i,j)^.shuffled
--- @
---
--- @
--- 'shuffled' . 'unshuffled' = 'id'
--- 'unshuffled' . 'shuffled' = 'id'
--- @
-shuffled :: Iso' (Word32, Word32) Key
-shuffled = iso (uncurry key) unshuffle
-{-# INLINE shuffled #-}
-
--- | This isomorphism lets you build a pair of indices from a key.
-unshuffled :: Iso' Key (Word32, Word32)
-unshuffled = iso unshuffle (uncurry key)
-{-# INLINE unshuffled #-}
-
-unshuffle :: Key -> (Word32, Word32)
-unshuffle (Key k0) = (fromIntegral (unsafeShiftR k5 32), fromIntegral k5) where
-  t0 = xor k0 (unsafeShiftR k0 1 ) .&. 0x2222222222222222
-  k1 = k0 `xor` t0 `xor` unsafeShiftL t0 1
-  t1 = xor k1 (unsafeShiftR k1 2 ) .&. 0x0C0C0C0C0C0C0C0C
-  k2 = k1 `xor` t1 `xor` unsafeShiftL t1 2
-  t2 = xor k2 (unsafeShiftR k2 4 ) .&. 0x00F000F000F000F0
-  k3 = k2 `xor` t2 `xor` unsafeShiftL t2 4
-  t3 = xor k3 (unsafeShiftR k3 8 ) .&. 0x0000FF000000FF00
-  k4 = k3 `xor` t3 `xor` unsafeShiftL t3 8
-  t4 = xor k4 (unsafeShiftR k4 16) .&. 0x00000000FFFF0000
-  k5 = k4 `xor` t4 `xor` unsafeShiftL t4 16
-{-# INLINE unshuffle #-}
-
 instance (a ~ Word32, b ~ Word32) => Field1 Key Key a b where
-  _1 f (Key ij) = indexed f (0 :: Int) (fromIntegral k5) <&> \i -> let
-         i0 = fromIntegral i
-         i1 = unsafeShiftL (i0 .&. 0x00000000FFFF0000) 16 .|. i0 .&. 0xFFFF00000000FFFF
-         i2 = unsafeShiftL (i1 .&. 0x0000FF000000FF00) 8  .|. i1 .&. 0xFF0000FFFF0000FF
-         i3 = unsafeShiftL (i2 .&. 0x00F000F000F000F0) 4  .|. i2 .&. 0xF00FF00FF00FF00F
-         i4 = unsafeShiftL (i3 .&. 0x0C0C0C0C0C0C0C0C) 2  .|. i3 .&. 0xC3C3C3C3C3C3C3C3
-         i5 = unsafeShiftL (i4 .&. 0x2222222222222222) 1  .|. i4 .&. 0x9999999999999999
-      in Key (unsafeShiftL i5 1 .|. ij .&. m2)
-    where
-      k0 = unsafeShiftR (ij .&. m1) 1
-      k1 = (unsafeShiftR k0 1  .|. k0) .&. 0x3333333333333333
-      k2 = (unsafeShiftR k1 2  .|. k1) .&. 0x0F0F0F0F0F0F0F0F
-      k3 = (unsafeShiftR k2 4  .|. k2) .&. 0x00FF00FF00FF00FF
-      k4 = (unsafeShiftR k3 8  .|. k3) .&. 0x0000FFFF0000FFFF
-      k5 = (unsafeShiftR k4 16 .|. k4) .&. 0x00000000FFFFFFFF
-  -- _1 = unshuffled._1 -- reference implementation
+  _1 f (Key i j) = indexed f (0 :: Int) i <&> \i' -> Key i' j
   {-# INLINE _1 #-}
 
 instance (a ~ Word32, b ~ Word32) => Field2 Key Key a b where
-  _2 f (Key ij) = indexed f (1 :: Int) (fromIntegral k5) <&> \j -> let
-         j0 = fromIntegral j
-         j1 = unsafeShiftL (j0 .&. 0x00000000FFFF0000) 16 .|. j0 .&. 0xFFFF00000000FFFF
-         j2 = unsafeShiftL (j1 .&. 0x0000FF000000FF00) 8  .|. j1 .&. 0xFF0000FFFF0000FF
-         j3 = unsafeShiftL (j2 .&. 0x00F000F000F000F0) 4  .|. j2 .&. 0xF00FF00FF00FF00F
-         j4 = unsafeShiftL (j3 .&. 0x0C0C0C0C0C0C0C0C) 2  .|. j3 .&. 0xC3C3C3C3C3C3C3C3
-         j5 = unsafeShiftL (j4 .&. 0x2222222222222222) 1  .|. j4 .&. 0x9999999999999999
-      in Key (ij .&. m1 .|. j5)
-    where
-      k0 = ij .&. m2
-      k1 = (unsafeShiftR k0 1  .|. k0) .&. 0x3333333333333333
-      k2 = (unsafeShiftR k1 2  .|. k1) .&. 0x0F0F0F0F0F0F0F0F
-      k3 = (unsafeShiftR k2 4  .|. k2) .&. 0x00FF00FF00FF00FF
-      k4 = (unsafeShiftR k3 8  .|. k3) .&. 0x0000FFFF0000FFFF
-      k5 = (unsafeShiftR k4 16 .|. k4) .&. 0x00000000FFFFFFFF
-  -- _2 = unshuffled._2 -- reference implementation
+  _2 f (Key i j) = indexed f (1 :: Int) j <&> Key i
   {-# INLINE _2 #-}
 
-instance Show Key where
-  showsPrec d w = case unshuffle w of
-    (i,j) -> showParen (d > 10) $
-      showString "key " . Prelude.showsPrec 11 i .
-           showChar ' ' . Prelude.showsPrec 11 j
+instance U.Unbox Key
 
-instance Read Key where
-  readsPrec d = readParen (d > 10) $ \r ->
-    [ (key i j, u)
-    | ("key",s) <- lex r
-    , (i,t) <- readsPrec 11 s
-    , (j,u) <- readsPrec 11 t
-    ]
+newtype instance U.MVector s Key = MV_Key (U.MVector s (Word32,Word32))
+newtype instance U.Vector    Key = V_Key (U.Vector (Word32,Word32))
 
--- * Utilities
+instance GM.MVector U.MVector Key where
+  {-# INLINE basicLength #-}
+  {-# INLINE basicUnsafeSlice #-}
+  {-# INLINE basicOverlaps #-}
+  {-# INLINE basicUnsafeNew #-}
+  {-# INLINE basicUnsafeReplicate #-}
+  {-# INLINE basicUnsafeRead #-}
+  {-# INLINE basicUnsafeWrite #-}
+  {-# INLINE basicClear #-}
+  {-# INLINE basicSet #-}
+  {-# INLINE basicUnsafeCopy #-}
+  {-# INLINE basicUnsafeGrow #-}
+  basicLength (MV_Key v) = GM.basicLength v
+  basicUnsafeSlice i n (MV_Key v) = MV_Key $ GM.basicUnsafeSlice i n v
+  basicOverlaps (MV_Key v1) (MV_Key v2) = GM.basicOverlaps v1 v2
+  basicUnsafeNew n = MV_Key `liftM` GM.basicUnsafeNew n
+  basicUnsafeReplicate n (Key x y) = MV_Key `liftM` GM.basicUnsafeReplicate n (x,y)
+  basicUnsafeRead (MV_Key v) i = uncurry Key `liftM` GM.basicUnsafeRead v i
+  basicUnsafeWrite (MV_Key v) i (Key x y) = GM.basicUnsafeWrite v i (x,y)
+  basicClear (MV_Key v) = GM.basicClear v
+  basicSet (MV_Key v) (Key x y) = GM.basicSet v (x,y)
+  basicUnsafeCopy (MV_Key v1) (MV_Key v2) = GM.basicUnsafeCopy v1 v2
+  basicUnsafeMove (MV_Key v1) (MV_Key v2) = GM.basicUnsafeMove v1 v2
+  basicUnsafeGrow (MV_Key v) n = MV_Key `liftM` GM.basicUnsafeGrow v n
 
--- | Masks for the interleaved components of a key
-m1, m2 :: Word64
-m1 = 0xAAAAAAAAAAAAAAAA
-m2 = 0x5555555555555555
-{-# INLINE m1 #-}
-{-# INLINE m2 #-}
+instance G.Vector U.Vector Key where
+
+  {-# INLINE basicUnsafeFreeze #-}
+  {-# INLINE basicUnsafeThaw #-}
+  {-# INLINE basicLength #-}
+  {-# INLINE basicUnsafeSlice #-}
+  {-# INLINE basicUnsafeIndexM #-}
+  {-# INLINE elemseq #-}
+  basicUnsafeFreeze (MV_Key v) = V_Key `liftM` G.basicUnsafeFreeze v
+  basicUnsafeThaw (V_Key v) = MV_Key `liftM` G.basicUnsafeThaw v
+  basicLength (V_Key v) = G.basicLength v
+  basicUnsafeSlice i n (V_Key v) = V_Key $ G.basicUnsafeSlice i n v
+  basicUnsafeIndexM (V_Key v) i = uncurry Key `liftM` G.basicUnsafeIndexM v i
+  basicUnsafeCopy (MV_Key mv) (V_Key v) = G.basicUnsafeCopy mv v
+  elemseq _ (Key x y) z = G.elemseq (undefined :: U.Vector Word32) x
+                        $ G.elemseq (undefined :: U.Vector Word32) y z
