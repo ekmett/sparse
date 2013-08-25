cb0465cd6674a10fe3f91dd25caab92760864f4c
Edward Kmett <ekmett@gmail.com>
2013-08-25 01:05:36 -0400

    added some larger benchmarks

--- benchmarks/mm.hs
+++ benchmarks/mm.hs
@@ -1,3 +1,4 @@
+import Control.Applicative
 import Control.DeepSeq
 import Criterion.Main
 import Data.Array.Unboxed as A
@@ -20,13 +21,22 @@ main = defaultMain
   , bench "naive 32x32"  $ nf (\x -> mmul x x) $ listArray ((0,0),(31,31)) $ Prelude.replicate (32*32) 1
   , bench "32x32 Int"    $ nf (\x -> x * x) blockInt
   , bench "32x32 ()"     $ nf (\x -> multiplyWith const (Heap.streamHeapWith const) x x) blockUnit
+  , bench "naive 128x128"  $ nf (\x -> mmul x x) $ listArray ((0,0),(127,127)) $ Prelude.replicate (128*128) 1
+  , bench "128x128 Int"    $ nf (\x -> x * x) blockInt128
+  , bench "128x128 ()"     $ nf (\x -> multiplyWith const (Heap.streamHeapWith const) x x) blockUnit128
   ]
 
 blockInt :: Mat U.Vector Int
-blockInt = M.fromList $ Prelude.zip [key 0 0..key 31 31] (repeat 1)
+blockInt = M.fromList $ Prelude.zip (Key <$> [0..31] <*> [0..31]) (repeat 1)
+
+blockInt128 :: Mat U.Vector Int
+blockInt128 = M.fromList $ Prelude.zip (Key <$> [0..127] <*> [0..127]) (repeat 1)
 
 blockUnit :: Mat U.Vector ()
-blockUnit = M.fromList $ Prelude.zip [key 0 0..key 31 31] (repeat ())
+blockUnit = M.fromList $ Prelude.zip (Key <$> [0..31] <*> [0..31]) (repeat ())
+
+blockUnit128 :: Mat U.Vector ()
+blockUnit128 = M.fromList $ Prelude.zip (Key <$> [0..127] <*> [0..127]) (repeat ())
 
 mmul :: UArray (Int,Int) Int -> UArray (Int,Int) Int -> UArray (Int,Int) Int
 mmul x y = accumArray (+) 0 ((i0,k0),(i1,k1)) $ do
