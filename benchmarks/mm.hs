63674b2ddb57b2154f46e4caa64aeae3eff0faa6
Edward Kmett <ekmett@gmail.com>
2013-08-25 11:48:08 -0400

    switch to 64x64 to get through benchmarks in a more timely manner

--- benchmarks/mm.hs
+++ benchmarks/mm.hs
@@ -20,22 +20,22 @@ main = defaultMain
   , bench "naive 32x32"  $ nf (\x -> mmul x x) $ listArray ((0,0),(31,31)) $ Prelude.replicate (32*32) 1
   , bench "32x32 Int"    $ nf (\x -> x * x) blockInt
   , bench "32x32 ()"     $ nf (\x -> multiplyWith const (Heap.streamHeapWith const) x x) blockUnit
-  , bench "naive 128x128"  $ nf (\x -> mmul x x) $ listArray ((0,0),(127,127)) $ Prelude.replicate (128*128) 1
-  , bench "128x128 Int"    $ nf (\x -> x * x) blockInt128
-  , bench "128x128 ()"     $ nf (\x -> multiplyWith const (Heap.streamHeapWith const) x x) blockUnit128
+  , bench "naive 64x64"  $ nf (\x -> mmul x x) $ listArray ((0,0),(63,63)) $ Prelude.replicate (64*64) 1
+  , bench "64x64 Int"    $ nf (\x -> x * x) blockInt64
+  , bench "64x64 ()"     $ nf (\x -> multiplyWith const (Heap.streamHeapWith const) x x) blockUnit64
   ]
 
 blockInt :: Mat Int
 blockInt = M.fromList $ Prelude.zip (Key <$> [0..31] <*> [0..31]) (repeat 1)
 
-blockInt128 :: Mat Int
-blockInt128 = M.fromList $ Prelude.zip (Key <$> [0..127] <*> [0..127]) (repeat 1)
+blockInt64 :: Mat Int
+blockInt64 = M.fromList $ Prelude.zip (Key <$> [0..63] <*> [0..63]) (repeat 1)
 
 blockUnit :: Mat ()
 blockUnit = M.fromList $ Prelude.zip (Key <$> [0..31] <*> [0..31]) (repeat ())
 
-blockUnit128 :: Mat ()
-blockUnit128 = M.fromList $ Prelude.zip (Key <$> [0..127] <*> [0..127]) (repeat ())
+blockUnit64 :: Mat ()
+blockUnit64 = M.fromList $ Prelude.zip (Key <$> [0..63] <*> [0..63]) (repeat ())
 
 mmul :: UArray (Int,Int) Int -> UArray (Int,Int) Int -> UArray (Int,Int) Int
 mmul x y = accumArray (+) 0 ((i0,k0),(i1,k1)) $ do
